65169cc5bf19e12f7f6e4d579b2ff8a7495b4908
krdelva <kevin.r.delva@gmail.com>
2022-09-12 14:43:05 -0400

    Change tempValue to constant and reorganize code minimally

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,12 @@
 const sumAll = function(initialValue, endValue) {
     if (initialValue < 0 || endValue < 0 || typeof initialValue !== "number" || typeof endValue !== "number") return "ERROR"
-    let result = 0;
     if (initialValue > endValue) {
-        let tempValue = endValue;
+        const tempValue = endValue;
         endValue = initialValue;
         initialValue = tempValue;
     }
 
+    let result = 0;
     for (let i = initialValue; i <= endValue; i++) {
         result += i;
     }
