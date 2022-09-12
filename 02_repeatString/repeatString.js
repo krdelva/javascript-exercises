dae12991f98ca3fa2c78a87a999b6a83df5a13b1
krdelva <kevin.r.delva@gmail.com>
2022-09-12 11:47:17 -0400

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    //console.log(`${word} | ${times}`)
+    let result = "";
+    if (times < 0) return "ERROR"
+    for (let i = 0; i < times; i++) {
+        result += word;
+    }
+    return result;
 };
 
 // Do not edit below this line
