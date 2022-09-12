772dafd25959f4755f51585dc9b70f32dd965808
krdelva <kevin.r.delva@gmail.com>
2022-09-12 12:19:59 -0400

    Remove unnecessary comments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,8 @@
 const removeFromArray = function(arr, ...toRemove) {
-    //console.log(`${arr} || ${toRemove}`)
     for (const n of toRemove) {
         arr = arr.filter(ele => ele !== n)
     }
     return arr;
-    //return arr.filter(ele => ele !== toRemove)
 };
 
 // Do not edit below this line
