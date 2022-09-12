751b9340b54e07d5809c29ee401b09bf615977b1
krdelva <kevin.r.delva@gmail.com>
2022-09-12 14:41:00 -0400

    Solve exercise 05 with conditionals and loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(initialValue, endValue) {
+    if (initialValue < 0 || endValue < 0 || typeof initialValue !== "number" || typeof endValue !== "number") return "ERROR"
+    let result = 0;
+    if (initialValue > endValue) {
+        let tempValue = endValue;
+        endValue = initialValue;
+        initialValue = tempValue;
+    }
 
+    for (let i = initialValue; i <= endValue; i++) {
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
