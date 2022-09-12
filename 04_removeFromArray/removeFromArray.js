6042c81772c08afb1625c4f116b5f064d3ac3de5
krdelva <kevin.r.delva@gmail.com>
2022-09-12 12:18:58 -0400

    Complete exercise 04 with Array.filter & looping through multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...toRemove) {
+    //console.log(`${arr} || ${toRemove}`)
+    for (const n of toRemove) {
+        arr = arr.filter(ele => ele !== n)
+    }
+    return arr;
+    //return arr.filter(ele => ele !== toRemove)
 };
 
 // Do not edit below this line
