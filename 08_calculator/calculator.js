2f683814f42194a3cddde9f2f830639a5dfeee6f
krdelva <kevin.r.delva@gmail.com>
2022-11-09 14:27:44 -0400

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length == 0)
+    return 0;
+  
+  return arr.reduce((acc, current) => acc + current)
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr.length == 0)
+    return 0;
 
+  return arr.reduce((acc, current) => acc * current);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a == 0) return 1;
+  if (a == 1) return 1;
+
+  return a * factorial(a - 1);
+
 };
 
 // Do not edit below this line
